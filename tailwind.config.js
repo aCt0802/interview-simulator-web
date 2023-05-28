53be252f9d0d48e283946458dba07555d493b315
aCt0802 <0802.kaz@gmail.com>
2023-05-28 21:56:02 +0900

    カラーテーマを変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          100: "var(--color-primary-100)",
-          200: "var(--color-primary-200)",
-          300: "var(--color-primary-300)",
-          400: "var(--color-primary-400)",
-          500: "var(--color-primary-500)"
+        backGround: "#232946",
+        accent: "#fffffe",
+        general: "#b8c1ec",
+        component: {
+          general: "#eebbc3",
+          accent: "#232946"
         }
       }
     },
